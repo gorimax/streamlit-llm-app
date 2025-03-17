5a9306cf23f874d99242634439eae69fa2461a0d
gorimax <trek6117@gmail.com>
2025-03-18 00:26:12 +0900

    adding sample2.py

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ load_dotenv()
 Webアプリの概要や操作方法をユーザーに明示するためのテキストを表示してください。
 Streamlit Community Cloudにデプロイする際、Pythonのバージョンは「3.11」としてください。
 """
+
+"""
+Run Command
+streamlit run sample1.py
+"""
+
+import streamlit as st
+st.title("サンプルアプリ①: 簡単なWebアプリ")
+input_message = st.text_input(label="文字数のカウント対象となるテキストを入力してください。")
+text_count = len(input_message)
+if st.button("実行"):
+    st.write(f"文字数: **{text_count}**")
